9a70923d4ded91713e0fe971d83a625d4ea931a5
cielavenir <cielartisan@gmail.com>
2016-12-28 22:58:12 +0900

    re-rename

--- twintail.rb
+++ twintail.rb
@@ -2,14 +2,14 @@
 # Older website (dead): http://air-lang.sourceforge.jp/
 # Older manual: https://www20.atwiki.jp/air-lang/
 
-class TwinTail < Formula
+class Twintail < Formula
 	desc 'Programming Language AIR / TwinTail'
 	homepage 'http://angelmode.sourceforge.jp/'
 	url 'https://osdn.net/projects/angelmode/downloads/59899/twintail_de_angelmode.costume146.tgz'
 	sha256 'a56fbb6fe2f9629e0ae205d310262e210cb444c2c29eb8a35687950b1b9aab85'
 
 	patch :p0 do
-		url 'http://raw.githubusercontent.com/cielavenir/homebrew-ciel/master/patch/tt.patch'
+		url 'http://raw.githubusercontent.com/cielavenir/homebrew-ciel/master/patch/twintail.patch'
 		sha256 'c66f6f7763c77a913c8e5f880f47778a5ddd46eacd37d6e8632c19188d20a8a1'
 	end
 
