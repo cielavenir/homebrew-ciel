00e014ca4e91f3eedf3c914e3893a1df65e76b57
cielavenir <cielartisan@gmail.com>
2017-06-08 08:02:40 +0900

    twintail source patch is now run automatically

--- twintail.rb
+++ twintail.rb
@@ -5,15 +5,16 @@
 class Twintail < Formula
 	desc 'Programming Language AIR / TwinTail'
 	homepage 'http://angelmode.sourceforge.jp/'
-	url 'https://osdn.net/projects/angelmode/downloads/59899/twintail_de_angelmode.costume146.tgz'
-	sha256 'a56fbb6fe2f9629e0ae205d310262e210cb444c2c29eb8a35687950b1b9aab85'
+	url 'https://osdn.net/projects/angelmode/downloads/67782/twintail_de_angelmode.costume202.tgz'
+	sha256 '1a4dadadd2b9786b6a5fbcb7cd61f0d619a2b9e995f878a0633daa5d7f587800'
 
 	patch :p0 do
 		url 'http://raw.githubusercontent.com/cielavenir/homebrew-ciel/master/patch/twintail.patch'
-		sha256 'b6de0245fd40cae65a7f7b12df4be42777c258a994bbd7e563489ff5235702ae'
+		sha256 'b1b634a18005874b53d66ea63312a4945a1bc7d750e1a4864f5c1e6a004e07e4'
 	end
 
 	def install
+		system 'sed', '-i', '-e', 's/a->ival=WEXITSTATUS(system(x->str));/{int result=system(x->str);a->ival=WEXITSTATUS(result);}/', 'icode/proc.call/i_proc.c'
 		system 'make'
 		bin.install 'tt'
 	end
