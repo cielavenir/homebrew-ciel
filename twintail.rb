02d60e5ea36ebce137f15da23c89807cb6d6277d
cielavenir <cielartisan@gmail.com>
2018-05-07 17:30:50 +0900

    supported LinuxBrew running on FreeBSD

--- twintail.rb
+++ twintail.rb
@@ -2,19 +2,27 @@
 # Older website (dead): http://air-lang.sourceforge.jp/
 # Older manual: https://www20.atwiki.jp/air-lang/
 
-# Note: this formula requires OSX 10.11 (or later) to accept __MATH_LONG_DOUBLE_CONSTANTS.
+# Note: normal build requires OSX 10.11 (or later) to accept __MATH_LONG_DOUBLE_CONSTANTS. On <=10.10.x --with-oldlibm is required.
 
 class Twintail < Formula
 	desc 'Programming Language AIR / TwinTail'
 	homepage 'http://angelmode.sourceforge.jp/'
 	#url 'https://osdn.net/projects/angelmode/downloads/67782/twintail_de_angemode.costume___.tgz'
-	url 'http://dl.osdn.jp/angelmode/67782/twintail_de_angelmode.costume208.tgz'
-	sha256 '43d52ac31c6628a62efd8cfab325c39f8e97759d1608da29ab09e50b2119d0ea'
 
-	depends_on 'pkg-config' => :build
-	depends_on 'libatomic_ops'
+	if `uname`.chomp.end_with?('BSD')
+		# LinuxBrew running on FreeBSD ( https://qiita.com/cielavenir/items/741921fcecb281555f77 )
+		# libc can be very old so that 204 cannot be compiled.
+		# Also, --with-oldlibm is required.
+		url 'http://dl.osdn.jp/angelmode/67782/twintail_de_angelmode.costume203.tgz'
+		sha256 '48d3bab53a2a453954b45c4c3a5976c7174a00822d0d55834ee08c7530caf662'
+	else
+		url 'http://dl.osdn.jp/angelmode/67782/twintail_de_angelmode.costume208.tgz'
+		sha256 '43d52ac31c6628a62efd8cfab325c39f8e97759d1608da29ab09e50b2119d0ea'
+		depends_on 'pkg-config' => :build
+		depends_on 'libatomic_ops'
+	end
 
-	option 'with-oldlibm', 'Build with old libm, disabling tgmath support and long double'
+	option 'with-oldlibm', 'Build with old libm, disabling tgmath and long double support'
 	option 'with-gmtoff', 'Use tm_gmtoff rather than timezone global variable'
 
 	patch :p0 do
