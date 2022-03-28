f8847bb4d78dd2a37573da41f1fd89f3546d9082
cielavenir <cielartisan@gmail.com>
2022-03-28 10:11:20 +0900

    update xz patch detection

--- xz.rb
+++ xz.rb
@@ -9,16 +9,18 @@ class Xz < Formula
   url "https://downloads.sourceforge.net/project/lzmautils/xz-5.2.5.tar.gz"
   mirror "https://tukaani.org/xz/xz-5.2.5.tar.gz"
   sha256 "f6f4910fd033078738bd82bfba4f49219d03b17eb0794eb91efbae419f4aba10"
-  license all_of: [
-    :public_domain,
-    "LGPL-2.1-or-later",
-    "GPL-2.0-or-later",
-    "GPL-3.0-or-later",
-  ]
+  if respond_to?(:license)
+    license all_of: [
+      :public_domain,
+      "LGPL-2.1-or-later",
+      "GPL-2.0-or-later",
+      "GPL-3.0-or-later",
+    ]
+  end
 
   # patch liblzma for CentOS7
   # https://stackoverflow.com/a/50518491
-  if true # `uname`.chomp.end_with?('')
+  if `uname -r`.include?('.el7.')  # todo better check
     patch :p1 do
       url 'http://raw.githubusercontent.com/cielavenir/homebrew-ciel/master/patch/xz_centos7.patch'
       sha256 '0a65a3eb48ac42ed3a8d60079c1423465002b73bd2d89c55dd512c5c84381e38'
